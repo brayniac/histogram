b8e73a448c455a6928359387532e2d36c5dc903b
brayniac <brayniac@gmail.com>
2015-06-28 14:53:23 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![crate_type = "lib"]
+
+#![crate_name = "histogram"]
+
+/* 
+	This crate is inspired by HdrHistogram
+*/
+
+#[test]
+fn it_works() {
+}
+
+fn name(arg: typ) -> ret {
+    // add code here
+}
