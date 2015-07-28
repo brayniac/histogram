a714a6fb25dd3c4829329450c1e65f9299a01b81
brayniac <brayniac@gmail.com>
2015-07-27 19:52:51 -0700

    version 0.1.3
    - tweak index formula based on precomputed table
    - add record()
    - add merge()
    - expanded testing to guard against indexing issues

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,7 @@
 //! * pre-allocated datastructure
 //!
 //! # Future work
-//! * provide functionality to merge histograms
-//! * provide an iterator
+//! * unknown
 //!
 //! # Usage
 //!
@@ -218,6 +217,48 @@ impl Histogram {
         }
     }
 
+    /// record additional counts for value
+    ///
+    /// # Example
+    /// ```
+    /// # use histogram::{Histogram,HistogramConfig};
+    ///
+    /// let mut h = Histogram::new(
+    ///     HistogramConfig{
+    ///         max_memory: 0,
+    ///         max_value: 1000000,
+    ///         precision: 3,
+    /// }).unwrap();
+    ///
+    /// h.record(1, 1);
+    /// assert_eq!(h.get(1).unwrap(), 1);
+    ///
+    /// h.record(2, 2);
+    /// assert_eq!(h.get(2).unwrap(), 2);
+    ///
+    /// h.record(10, 10);
+    /// assert_eq!(h.get(10).unwrap(), 10);
+    pub fn record(&mut self, value: u64, count: u64) {
+        self.data.counters.entries_total = self.data.counters.entries_total.saturating_add(count);
+        if value < 1 {
+            self.data.counters.missed_small =
+                self.data.counters.missed_small.saturating_add(count);
+        } else if value > self.config.max_value {
+            self.data.counters.missed_large =
+                self.data.counters.missed_large.saturating_add(count);
+        } else {
+            match self.get_index(value) {
+                Some(index) => {
+                    self.data.data[index] = self.data.data[index].saturating_add(count);
+                },
+                None => {
+                    self.data.counters.missed_unknown =
+                        self.data.counters.missed_unknown.saturating_add(count);
+                }
+            }
+        }
+    }
+
     /// get the count for a value
     ///
     /// # Example
@@ -247,7 +288,7 @@ impl Histogram {
     fn get_index(&mut self, value: u64) -> Option<usize> {
         let result: Option<usize> = None;
 
-        if value >= 1 && value <= self.config.max_value {
+        if value >= 1 {
 
             if value <= self.properties.linear_max {
                 return Some((value - 1) as usize);
@@ -262,7 +303,10 @@ impl Histogram {
             let remain = value as f64 - 2.0_f64.powi(outer as i32);
 
             let inner = (self.properties.buckets_inner as f64 * remain as f64 /
-                         2.0_f64.powi((outer) as i32)).ceil() as u32;
+                         2.0_f64.powi((outer) as i32)).floor() as u32;
+
+            println!("Value: {} Outer: {} l_max: {} l_power: {} Remain: {} Inner: {}", value,
+                     outer, l_max, l_power, remain, inner);
 
             // this gives the shifted outer index
             let outer = outer as u32 - l_power;
@@ -295,7 +339,7 @@ impl Histogram {
         let mut value = 2.0_f64.powi((outer as u32 + self.properties.linear_power) as i32);
         value += inner as f64 * (value as f64 / self.properties.buckets_inner as f64);
 
-        value.floor() as u64
+        value.ceil() as u64
     }
 
     /// return the value for the given percentile
@@ -368,6 +412,51 @@ impl Histogram {
             }
         }
         None
+    }
+
+    /// merge one Histogram into another Histogram
+    ///
+    /// # Example
+    /// ```
+    /// # use histogram::{Histogram,HistogramConfig};
+    ///
+    /// let mut a = Histogram::new(
+    ///     HistogramConfig{
+    ///         max_memory: 0,
+    ///         max_value: 1000000,
+    ///         precision: 3,
+    /// }).unwrap();
+    ///
+    /// let mut b = Histogram::new(
+    ///     HistogramConfig{
+    ///         max_memory: 0,
+    ///         max_value: 1000000,
+    ///         precision: 3,
+    /// }).unwrap();
+    ///
+    /// assert_eq!(a.entries(), 0);
+    /// assert_eq!(b.entries(), 0);
+    ///
+    /// a.increment(1);
+    /// b.increment(2);
+    ///
+    /// assert_eq!(a.entries(), 1);
+    /// assert_eq!(b.entries(), 1);
+    ///
+    /// a.merge(&mut b);
+    ///
+    /// assert_eq!(a.entries(), 2);
+    /// assert_eq!(a.get(1).unwrap(), 1);
+    /// assert_eq!(a.get(2).unwrap(), 1);
+    pub fn merge(&mut self, other: &mut Histogram) {
+        loop {
+            match other.next() {
+                Some(bucket) => {
+                    self.record(bucket.value, bucket.count);
+                }
+                None => { break }
+            }
+        }
     }
 
     /// return the number of entries in the Histogram
@@ -553,16 +642,67 @@ mod tests {
 
         // this tests that rounding up within inner bucket works
         assert_eq!(h.get_index(16), Some(15));
-        assert_eq!(h.get_index(17), Some(16));
-        assert_eq!(h.get_index(18), Some(17));
-        assert_eq!(h.get_index(19), Some(17));
+        assert_eq!(h.get_index(17), Some(15));
+        assert_eq!(h.get_index(18), Some(16));
+        assert_eq!(h.get_index(19), Some(16));
 
         // these values prove roll-up into next outer bucket works
-        assert_eq!(h.get_index(62), Some(35));
-        assert_eq!(h.get_index(63), Some(35));
+        assert_eq!(h.get_index(62), Some(34));
+        assert_eq!(h.get_index(63), Some(34));
         assert_eq!(h.get_index(64), Some(35));
 
-        assert_eq!(h.get_index(65), Some(36));
+        assert_eq!(h.get_index(65), Some(35));
+    }
+
+    #[test]
+    fn test_get_index_2() {
+        // extensive test from precomputed table
+        let mut h = Histogram::new(HistogramConfig {
+            max_memory: 0,
+            max_value: 100,
+            precision: 1,
+        }).unwrap();
+
+        let v = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 23, 24, 26, 28, 29,
+            31, 32, 36, 39, 42, 45, 48, 52, 55, 58, 61, 64, 71, 77, 84, 90, 96, 103, 109, 116, 122
+        ];
+
+        for index in 0..45 {
+            let got = h.get_index(v[index]).unwrap();
+            assert!(got == index, "Value: {} Got: {} Want: {}", v[index], got, index);
+        }
+
+        for index in 0..45 {
+            let got = h.index_value(index);
+            assert!(got == v[index], "Index: {} Got: {} Want: {}", index, got, v[index]);
+        }
+    }
+
+    #[test]
+    fn test_get_index_3() {
+        // extensive test from precomputed table
+        let mut h = Histogram::new(HistogramConfig {
+            max_memory: 0,
+            max_value: 250,
+            precision: 1,
+        }).unwrap();
+
+        let v = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 23, 24, 26, 28, 29,
+            31, 32, 36, 39, 42, 45, 48, 52, 55, 58, 61, 64, 71, 77, 84, 90, 96, 103, 109, 116, 122,
+            128, 141, 154, 167, 180, 192, 205, 218, 231, 244
+        ];
+
+        for index in 0..55 {
+            let got = h.get_index(v[index]).unwrap();
+            assert!(got == index, "Value: {} Got: {} Want: {}", v[index], got, index);
+        }
+
+        for index in 0..55 {
+            let got = h.index_value(index);
+            assert!(got == v[index], "Index: {} Got: {} Want: {}", index, got, v[index]);
+        }
     }
 
     #[test]
@@ -594,7 +734,7 @@ mod tests {
             match h.next() {
                 Some(bucket) => {
                     match h.get_index(bucket.value) {
-                        Some(index) => {
+                        Some(_) => {
                             h.increment(bucket.value);
                         },
                         None => {},
